da3e54cc997953e3ba2924ce50af1305989f965d
Christopher Pedersen <chrisaped@gmail.com>
2013-04-25 18:23:50 -0700

    filled in views and controller for Question model

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,15 @@ class QuestionsController < ApplicationController
     # (1..10).sort { |a, b| b <=> a }  #=> [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
   end
 
+  def new
+  	@question = Question.new(params[:question])
+  end
+
+  def create
+  	@question = Question.new(params[:question])
+  	@question.user_id = current_user.id
+  	@question.save!
+  	redirect_to root_path
+  end
+
 end
